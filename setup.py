920fa85dfca857405c09e14c7648c48ecc1c1b5c
puneeth0309 <kottisapuneeth09@gmail.com>
2026-05-28 21:40:04 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages,setup
+
+setup(
+    name="Medical_Chatbot",
+    version="0.0.0",
+    author="Puneeth",
+    author_email="kottisapuneeth09@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
